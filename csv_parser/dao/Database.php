0f346f566d92f611cd83eb2d80ada5a2103f210b
sebastienM <seb.mandaba@gmail.com>
2018-03-08 14:08:06 +0100

    db insertion working

--- csv_parser/dao/Database.php
+++ csv_parser/dao/Database.php
@@ -10,7 +10,7 @@ namespace csv_parser\dao;
 require_once __DIR__ . '/../settings.php';
 
 use PDO;
-
+use Asso;
 
 class Database
 {
@@ -31,5 +31,101 @@ class Database
             self::$instance = new Database();
         }
         return self::$instance->em;
+    }
+
+    public static function add_asso(\Asso $asso,
+                             $code_tiers,
+                             $libelle_tiers,
+                             $sigle,
+                             $descriptif,
+                             $rue1,
+                             $rue2,
+                             $cp,
+                             $ville,
+                             $telephone,
+                             $mail,
+                             $site_internet,
+                             $code_categorie,
+                             $libelle_categorie,
+                             $code_sous_categorie,
+                             $libelle_sous_categorie,
+                             $site,
+                             $adresse_site,
+                             $ouverture,
+                             $permanence,
+                             $public_vise)
+    {
+        $em = self::instance();
+        $query = $em->prepare("INSERT INTO `annuaire_assos`(CODE_TIERS,
+                                                              LIBELLE_TIERS,
+                                                              SIGLE,
+                                                              DESCRIPTIF,
+                                                              RUE1,
+                                                              RUE2,
+                                                              CP,
+                                                              VILLE,
+                                                              TELEPHONE,
+                                                              MAIL,
+                                                              SITE_INTERNET,
+                                                              CODE_CATEGORIE,
+                                                              LIBELLE_CATEGORIE,
+                                                              CODE_SOUS_CATEGORIE,
+                                                              LIBELLE_SOUS_CATEGORIE,
+                                                              SITE,
+                                                              ADRESSE_SITE,
+                                                              OUVERTURE,
+                                                              PERMANENCE,
+                                                              PUBLIC_VISE,
+                                                              tag) 
+                                                              VALUES (:CODE_TIERS,
+                                                                  :LIBELLE_TIERS,
+                                                                  :SIGLE,
+                                                                  :DESCRIPTIF,
+                                                                  :RUE1,
+                                                                  :RUE2,
+                                                                  :CP,
+                                                                  :VILLE,
+                                                                  :TELEPHONE,
+                                                                  :MAIL,
+                                                                  :SITE_INTERNET,
+                                                                  :CODE_CATEGORIE,
+                                                                  :LIBELLE_CATEGORIE,
+                                                                  :CODE_SOUS_CATEGORIE,
+                                                                  :LIBELLE_SOUS_CATEGORIE,
+                                                                  :SITE,
+                                                                  :ADRESSE_SITE,
+                                                                  :OUVERTURE,
+                                                                  :PERMANENCE,
+                                                                  :PUBLIC_VISE,
+                                                                  :tag)");
+        $query->bindValue(':CODE_TIERS',$code_tiers);
+        $query->bindValue(':LIBELLE_TIERS',$libelle_tiers);
+        $query->bindValue(':SIGLE',$sigle);
+        $query->bindValue(':DESCRIPTIF',$descriptif);
+        $query->bindValue(':RUE1',$rue1);
+        $query->bindValue(':RUE2',$rue2);
+        $query->bindValue(':CP',$cp);
+        $query->bindValue(':VILLE',$ville);
+        $query->bindValue(':TELEPHONE',$telephone);
+        $query->bindValue(':MAIL',$mail);
+        $query->bindValue(':SITE_INTERNET',$site_internet);
+        $query->bindValue(':CODE_CATEGORIE',$code_categorie);
+        $query->bindValue(':LIBELLE_CATEGORIE',$libelle_categorie);
+        $query->bindValue(':CODE_SOUS_CATEGORIE',$code_sous_categorie);
+        $query->bindValue(':LIBELLE_SOUS_CATEGORIE',$libelle_sous_categorie);
+        $query->bindValue(':SITE',$site);
+        $query->bindValue(':ADRESSE_SITE',$adresse_site);
+        $query->bindValue(':OUVERTURE',$ouverture);
+        $query->bindValue(':PERMANENCE',$permanence);
+        $query->bindValue(':PUBLIC_VISE',$public_vise);
+        $query->bindValue(':tag',$asso->getLIBELLETIERS());
+        try{
+            $query->execute();
+            return true;
+        }
+        catch (\Exception $e){
+//            echo $e;
+            return false;
+        }
     }
 }
--- csv_parser/import_csv.php
+++ csv_parser/import_csv.php
@@ -12,77 +12,34 @@ require_once './dao/Database.php';
 use csv_parser\dao\Database;
 
 $data_getter = new GetData('../assocs.csv');
-$em = Database::instance();
 $assos = $data_getter->get_assos();
 foreach ($assos as $asso) {
-    $query = $em->prepare("INSERT INTO `annuaire_assos`(CODE_TIERS,
-                                                              LIBELLE_TIERS,
-                                                              SIGLE,
-                                                              DESCRIPTIF,
-                                                              RUE1,
-                                                              RUE2,
-                                                              CP,
-                                                              VILLE,
-                                                              TELEPHONE,
-                                                              MAIL,
-                                                              SITE_INTERNET,
-                                                              CODE_CATEGORIE,
-                                                              LIBELLE_CATEGORIE,
-                                                              CODE_SOUS_CATEGORIE,
-                                                              LIBELLE_SOUS_CATEGORIE,
-                                                              SITE,
-                                                              ADRESSE_SITE,
-                                                              OUVERTURE,
-                                                              PERMANENCE,
-                                                              PUBLIC_VISE,
-                                                              tag) 
-                                                              VALUES (:CODE_TIERS,
-                                                                  :LIBELLE_TIERS,
-                                                                  :SIGLE,
-                                                                  :DESCRIPTIF,
-                                                                  :RUE1,
-                                                                  :RUE2,
-                                                                  :CP,
-                                                                  :VILLE,
-                                                                  :TELEPHONE,
-                                                                  :MAIL,
-                                                                  :SITE_INTERNET,
-                                                                  :CODE_CATEGORIE,
-                                                                  :LIBELLE_CATEGORIE,
-                                                                  :CODE_SOUS_CATEGORIE,
-                                                                  :LIBELLE_SOUS_CATEGORIE,
-                                                                  :SITE,
-                                                                  :ADRESSE_SITE,
-                                                                  :OUVERTURE,
-                                                                  :PERMANENCE,
-                                                                  :PUBLIC_VISE,
-                                                                  :tag)");
-    $query->bindValue(':CODE_TIERS', $asso->getCODETIERS());
-    $query->bindValue(':LIBELLE_TIERS', $asso->getLIBELLETIERS());
-    $query->bindValue(':SIGLE', $asso->getSIGLE());
-    $query->bindValue(':DESCRIPTIF', $asso->getDESCRIPTIF());
-    $query->bindValue(':RUE1', $asso->getRUE1());
-    $query->bindValue(':RUE2', $asso->getRUE2());
-    $query->bindValue(':CP', $asso->getCP());
-    $query->bindValue(':VILLE', $asso->getVILLE());
-    $query->bindValue(':TELEPHONE', $asso->getTELEPHONE());
-    $query->bindValue(':MAIL', $asso->getMAIL());
-    $query->bindValue(':SITE_INTERNET', $asso->getSITEINTERNET());
-    $query->bindValue(':CODE_CATEGORIE', $asso->getCODECATEGORIE());
-    $query->bindValue(':LIBELLE_CATEGORIE', $asso->getLIBELLECATEGORIE());
-    $query->bindValue(':CODE_SOUS_CATEGORIE', $asso->getCODESOUSCATEGORIE());
-    $query->bindValue(':LIBELLE_SOUS_CATEGORIE', $asso->getLIBELLESOUSCATEGORIE());
-    $query->bindValue(':SITE', $asso->getSITE());
-    $query->bindValue(':ADRESSE_SITE', $asso->getADRESSESITE());
-    $query->bindValue(':OUVERTURE', $asso->getOUVERTURE());
-    $query->bindValue(':PERMANENCE', $asso->getPERMANENCE());
-    $query->bindValue(':PUBLIC_VISE', $asso->getPUBLICVISE());
-    $query->bindValue(':tag', $asso->getTAG($asso->getLIBELLETIERS()));
-    try{
-        $query->execute();
+    $query = Database::add_asso($asso,
+        $asso->getCODETIERS(),
+        $asso->getLIBELLETIERS(),
+        $asso->getSIGLE(),
+        $asso->getDESCRIPTIF(),
+        $asso->getRUE1(),
+        $asso->getRUE2(),
+        $asso->getCP(),
+        $asso->getVILLE(),
+        $asso->getTELEPHONE(),
+        $asso->getMAIL(),
+        $asso->getSITEINTERNET(),
+        $asso->getCODECATEGORIE(),
+        $asso->getLIBELLECATEGORIE(),
+        $asso->getCODESOUSCATEGORIE(),
+        $asso->getLIBELLESOUSCATEGORIE(),
+        $asso->getSITE(),
+        $asso->getADRESSESITE(),
+        $asso->getOUVERTURE(),
+        $asso->getPERMANENCE(),
+        $asso->getPUBLICVISE());
+    if($query){
+        echo 'ok';
     }
-    catch (\Exception $e){
-        echo $e;
+    else{
+        echo 'error';
     }
-//    $query->execute();
+
 }
